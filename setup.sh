7abf67ac91a967a68a0c12fa401ad25e9c55493a
buckn <nathanieljbuck7@gmail.com>
2019-02-11 13:01:07 -0500

    third commit

--- setup.sh
+++ setup.sh
@@ -9,24 +9,12 @@ sudo add-apt-repository multiverse
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i ./discord.deb
 
-sudo apt-get install openbox
-sudo apt-get install lightdm
-sudo apt-get install lxde
-sudo apt-get install nnn
-sudo apt-get install firefox
-sudo apt-get install cmus
-sudo apt-get install conky-all
-sudo apt-get install nestopia
-sudo apt-get install ppsspp
-sudo apt-get install sublime-text
-sudo apt-get install gzdoom
-sudo apt-get install gimp
-sudo apt-get install steam-installer
-sudo apt-get install mpv
-sudo apt-get install gpick
-sudo apt-get install nethack
+sudo apt-get install openbox lightdm lxqt nnn firefox cmus conky-all nestopia ppsspp sublime-text gzdoom gimp steam-installer mpv gpick nethack
 
 sudo apt-get purge chromium*
+
+sudo apt-get upgrade
+sudo apt-get autoremove
 
 echo -e "@pcmanfm --desktop --profile LXDE\n@conky" > ~/.config/lxsession/LXDE/autostart
 cp ./lxe-rc.xml ~/.config/openbox/
