c1a3a9a095e5debf2df1e4e92015021939f8ea53
buckn <nathanieljbuck7@gmail.com>
2019-02-11 18:20:08 -0500

    fifth commit

--- setup.sh
+++ setup.sh
@@ -6,25 +6,14 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-add-repository 'deb http://debian.drdteam.org/ stable multiverse'
 sudo add-apt-repository multiverse
 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i ./discord.deb
 
-sudo apt-get install -f openbox
-sudo apt-get install -f lightdm
-sudo apt-get install -f lxqt
-sudo apt-get install -f nnn
-sudo apt-get install -f firefox
-sudo apt-get install -f cmus
-sudo apt-get install -f conky-all
-sudo apt-get install -f nestopia
-sudo apt-get install -f ppsspp
-sudo apt-get install -f sublime-text
-sudo apt-get install -f gzdoom
-sudo apt-get install -f gimp
-sudo apt-get install -f steam-installer
-sudo apt-get install -f mpv
-sudo apt-get install -f gpick
-sudo apt-get install -f nethack
+sudo apt-get install -fy openbox lightdm lxqt nnn firefox cmus conky-all nestopia ppsspp sublime-text gzdoom gimp steam-installer mpv gpick nethack
 
 sudo apt-get purge chromium*
 
@@ -33,4 +22,6 @@ sudo apt-get autoremove
 
 echo -e "@pcmanfm --desktop --profile LXDE\n@conky" > ~/.config/lxsession/LXDE/autostart
 cp ./lxe-rc.xml ~/.config/openbox/
-`cp ./conkyrc ~/.conkyrc
+cp ./conkyrc ~/.conkyrc
+
+sudo reboot
