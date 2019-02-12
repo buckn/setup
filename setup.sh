6c3585e4e1cc6b93ef307b44aa66ed9f80d88a16
buckn <nathanieljbuck7@gmail.com>
2019-02-12 16:13:28 -0500

    fixed discord and steam install, removed gzdoom

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash	
 
+sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:agornostal/ulauncher
 add-apt-repository ppa:ppsspp/stable
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
-apt-add-repository 'deb http://debian.drdteam.org/ stable multiverse'
 add-apt-repository multiverse
 
 apt-get update
@@ -12,12 +12,9 @@ apt-get upgrade
 
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 dpkg -i ./discord.deb
+sudo apt-get install -f
 
-apt-get install -y openbox lightdm lxde nnn firefox cmus nestopia ppsspp sublime-text gzdoom  gimp steam-installer mpv gpick nethack ulauncher
-
-apt-get purge chromium*
-
-apt-get autoremove
+apt-get install -y lightdm lxde nnn firefox cmus nestopia ppsspp sublime-text gimp steam-installer mpv gpick nethack-console ulauncher screenfetch
 
 echo -e "@pcmanfm --desktop --profile LXDE" > ~/.config/lxsession/LXDE/autostart
 cp ./lxde-rc.xml ~/.config/openbox/lxde-rc.xml
